3bafe4453f6e079db42e26d3482f2203dccf7575
dengpengfei <dengpengfei@guazi.com>
2019-08-19 21:18:49 +0800

    子进程的标准输出输出到父进程中

--- scripts/nodemonPlugin.js
+++ scripts/nodemonPlugin.js
@@ -1,13 +1,17 @@
 const path = require('path')
 const execa = require('execa')
 
+let flag = true
+
 class nodemonPlugin {
     // 应用函数
     apply (compiler) {
      // 绑定钩子事件
         compiler.plugin('after-emit', (compilation, cb) => {
+            if (!flag) return cb()
             setTimeout(() => {
-                execa('nodemon', [path.resolve(__dirname, '../dist/index.js')])                
+                flag = false
+                execa('nodemon', [path.resolve(__dirname, '../dist/index.js')]).stdout.pipe(process.stdout)
             }, 100)
             cb()
         })
