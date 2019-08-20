446d56454b050f619edce37c5c69ab884909839f
dengpengfei <dengpengfei@guazi.com>
2019-08-20 16:33:43 +0800

    修改代码自动重启node

--- scripts/nodemonPlugin.js
+++ scripts/nodemonPlugin.js
@@ -1,17 +1,18 @@
 const path = require('path')
 const execa = require('execa')
 
-let flag = true
+let subprocess
 
 class nodemonPlugin {
     // 应用函数
     apply (compiler) {
      // 绑定钩子事件
         compiler.plugin('after-emit', (compilation, cb) => {
-            if (!flag) return cb()
             setTimeout(() => {
-                flag = false
-                execa('nodemon', [path.resolve(__dirname, '../dist/index.js')]).stdout.pipe(process.stdout)
+                subprocess && subprocess.kill()
+                subprocess = execa('node', [path.resolve(__dirname, '../dist/index.js')])
+                subprocess.stdout.pipe(process.stdout)
+                subprocess.stderr.pipe(process.stderr)
             }, 100)
             cb()
         })
