5f5a61d77c8808eb7de393287dd64bee2432b925
dengpengfei <dengpengfei@guazi.com>
2019-08-29 19:36:25 +0800

    add comment

--- scripts/nodemonPlugin.js
+++ scripts/nodemonPlugin.js
@@ -9,9 +9,9 @@ class nodemonPlugin {
      // 绑定钩子事件
         compiler.plugin('after-emit', (compilation, cb) => {
             setTimeout(() => {
-                subprocess && subprocess.kill()
+                subprocess && subprocess.kill() // 修改代码后重新启动杀掉进程以便重启
                 subprocess = execa('node', [path.resolve(__dirname, '../dist/index.js')])
-                subprocess.stdout.pipe(process.stdout)
+                subprocess.stdout.pipe(process.stdout) // 子进程的标准输出流导入到父进程
                 subprocess.stderr.pipe(process.stderr)
             }, 100)
             cb()
