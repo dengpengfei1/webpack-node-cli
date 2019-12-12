2abccebb8ad1bf07d294422aeca1bd7d506eacc8
dengpengfei <dengpengfei@guazi.com>
2019-12-12 21:39:39 +0800

    git ignore

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 const { spawn } = require('child_process')
-const chalk = require('chalk')
 const path = require('path')
+const fs = require('fs')
+const chalk = require('chalk')
 
 var status = require('node-status')
  
@@ -16,12 +17,14 @@ if (!command) throw new Error(chalk.red('A COMMAND REQUIRED'))
 
 const func = {
   init() {
-    const cp = spawn('cp', ['-r', '-a', path.join(__dirname, '/code/'), process.env.PWD])
+    const cp = spawn('cp', ['-r', '-f', path.join(__dirname, '/code/'), process.env.PWD])
     cp.on('error', (err) => {
       console.log(chalk.red(err))
       process.exit()
     })
     cp.on('close', () => {
+      const gitignore = fs.readFileSync(path.join(__dirname, '/code/.gitignore'))
+      fs.writeFileSync(path.resolve(process.env.PWD, '.gitignore'), gitignore)
       console.log(chalk.green('复制文件完成！'))
       console.log(chalk.green('开始安装npm包'))
       const install = spawn('npm', ['i'])
